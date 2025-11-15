0fdb7a434fee9c270126dc6cae85afa4d4ce57f0
Bill Soronzonbold <Soronzonboldbill@gmail.com>
2025-11-15 14:41:46 -0500

    patch: add logout method on mobile sidebar

--- src/components/MobileNavSidebar.tsx
+++ src/components/MobileNavSidebar.tsx
@@ -9,6 +9,9 @@ import {
 } from "./ui/Tooltip";
 
 import { useNavbar } from "@/lib/contexts/NavSidebarContext";
+import { useAuth } from "@/lib/contexts/AuthContext";
+import { LogOut } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 type Profile = HelperProfileData | ClientProfileData;
 
@@ -20,6 +23,13 @@ type MobileNavSidebarProps = {
 
 export function MobileNavSidebar({ navigationItems, profile, isLoading }: MobileNavSidebarProps) {
   const { setPage } = useNavbar();
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate('/');
+  }
 
   return (
     <div className="fixed top-0 left-0 h-full w-16 bg-slate-100 z-10 border border-gray-200 py-4">
@@ -71,7 +81,44 @@ export function MobileNavSidebar({ navigationItems, profile, isLoading }: Mobile
             </TooltipProvider>
           ))}
         </div>
-        <div>
+        <div className="flex flex-col items-center justify-center gap-y-2">
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <div
+                  className="bg-slate-200 hover:bg-slate-300 transition-all duration-300 active:scale-80 hover:text-cyan-500 p-2 rounded-md"
+                  onClick={handleLogout}
+                >
+                  <LogOut strokeWidth={1.5} />
+                </div>
+              </TooltipTrigger>
+
+              <TooltipContent
+                className="z-10 transition-all duration-100 p-0 bg-transparent border-none shadow-none"
+                side="right"
+                align="center"
+                sideOffset={6}
+              >
+                <div className="flex flex-row items-center w-fit">
+                  {/* Triangle */}
+                  <div
+                    className="
+                        w-0 h-0
+                        border-t-12 border-b-12 border-r-12
+                        border-t-transparent border-b-transparent
+                        border-r-black
+                      "
+                  />
+
+                  {/* Text bubble */}
+                  <div className="w-fit text-nowrap py-1 pr-2 bg-black font-bold text-white rounded-r-md">
+                    Logout
+                  </div>
+
+                </div>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
           <div onClick={() => setPage("profile")} className="w-fit h-fit hover:cursor-pointer">
             {isLoading ? (
               <div className="size-10 rounded-full bg-linear-to-br from-indigo-500 to-purple-600 flex items-center justify-center border-4 border-white shadow-md">
@@ -89,7 +136,6 @@ export function MobileNavSidebar({ navigationItems, profile, isLoading }: Mobile
                   <span className="text-xl text-white">👤</span>
                 </div>
               )
-
             )}
           </div>
 
